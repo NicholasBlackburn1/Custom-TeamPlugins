3a0b037e5e0de340c9803d3a6602e92d6b8876b6
NICKY B <nickblackburn02@gmail.com>
2020-09-20 17:38:05 -0400

    added Project

--- src/main/java/space/nicholasblackburn/teams/Main.java
+++ src/main/java/space/nicholasblackburn/teams/Main.java
@@ -1,7 +1,23 @@
 package space.nicholasblackburn.teams;
 
+import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main{
+public class Main extends JavaPlugin {
 
-    
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onLoad() {
+       
+    }
+
+    @Override
+    public void onDisable() {
+      
+        
+    }
+
 }
